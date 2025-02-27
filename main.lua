9c82b35052d42201dcd33c0d644e078f115b1d01
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2025-02-27 20:30:48 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,5 +2,5 @@ while task.wait() do
 if workspace:FindFirstChild(game.Players.LocalPlayer.Name) and workspace[game.Players.LocalPlayer.Name]:FindFirstChild("Head") and workspace[game.Players.LocalPlayer.Name].Head:FindFirstChild("Nametag") then
 workspace[game.Players.LocalPlayer.Name].Head:FindFirstChild("Nametag"):Destroy()
 end
-game.Players.LocalPlayer.leaderstats.Slaps.Value = 696969
+game.Players.LocalPlayer.leaderstats.Slaps.Value = 696969
 end
