17ad901515a048a6a700be427502d913e24f854a
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2025-02-27 20:16:19 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
-while true do
-w = math.random(0, 1000000000)
-game.Players.LocalPlayer.Character.Head.Nametag.TextLabel.Text = w
-task.wait()
-l = math.random(0, 1000000)
-game.Players.LocalPlayer.leaderstats.Slaps.Value = l
+while task.wait() do
+if workspace:FindFirstChild(game.Players.LocalPlayer.Name) and workspace[game.Players.LocalPlayer.Name]:FindFirstChild("Head") and workspace[game.Players.LocalPlayer.Name].Head:FindFirstChild("Nametag") then
+workspace[game.Players.LocalPlayer.Name].Head:FindFirstChild("Nametag"):Destroy()
+end
+game.Players.LocalPlayer.leaderstats.Slaps.Value = 696969
 end
